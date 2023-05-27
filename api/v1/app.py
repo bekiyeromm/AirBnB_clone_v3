cbd1c0b66b8e54bfb9aee189298fb5a65b22e1ed
bekiyeromm <bk3tena@gmail.com>
2023-05-27 04:25:54 -0700

    In api/v1/app.py, create a handler for 404 errors that returns a JSON-formatted 404 status code response. The content should be: 'error':'Not found'

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,17 @@ def teardown_app(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def error_404(error):
+    '''handler for 404 errors that returns a
+    JSON-formatted 404 status code response
+    content should be: error: Not found
+    '''
+    resp = jsonify({"error": "Not found"})
+    resp.status_code = 404
+    return resp
+
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST')
     port = getenv('HBNB_API_PORT')
