72b03ab154c195928a823cfe70cac2e742a60344
bekiyeromm <bk3tena@gmail.com>
2023-05-29 13:12:43 -0700

    Create a new view for Review object that handles all default RESTFul API actions:
    In the file api/v1/views/places_reviews.py
    You must use to_dict() to retrieve an object into valid JSON
    Update api/v1/views/__init__.py to import this new file
    Retrieves the list of all Review objects of a Place: GET /api/v1/places/<place_id>/reviews
    
    If the place_id is not linked to any Place object, raise a 404 error
    Retrieves a Review object. : GET /api/v1/reviews/<review_id>
    
    If the review_id is not linked to any Review object, raise a 404 error
    Deletes a Review object: DELETE /api/v1/reviews/<review_id>
    
    If the review_id is not linked to any Review object, raise a 404 error
    Returns an empty dictionary with the status code 200
    Creates a Review: POST /api/v1/places/<place_id>/reviews
    
    You must use request.get_json from Flask to transform the HTTP request to a dictionary
    If the place_id is not linked to any Place object, raise a 404 error
    If the HTTP body request is not valid JSON, raise a 400 error with the message Not a JSON
    If the dictionary doesn’t contain the key user_id, raise a 400 error with the message Missing user_id
    If the user_id is not linked to any User object, raise a 404 error
    If the dictionary doesn’t contain the key text, raise a 400 error with the message Missing text
    Returns the new Review with the status code 201
    Updates a Review object: PUT /api/v1/reviews/<review_id>
    
    If the review_id is not linked to any Review object, raise a 404 error
    You must use request.get_json from Flask to transform the HTTP request to a dictionary
    If the HTTP request body is not valid JSON, raise a 400 error with the message Not a JSON
    Update the Review object with all key-value pairs of the dictionary
    Ignore keys: id, user_id, place_id, created_at and updated_at

---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""places_reviews"""
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.place import Place
+from models.review import Review
+
+
+@app_views.route('/places/<place_id>/reviews/', methods=['GET'],
+                 strict_slashes=False)
+def reviews_of_place(place_id):
+    '''lists of all Review objects of a Place '''
+    places = storage.all("Place").values()
+    place = [x.to_dict() for x in places if x.id == place_id]
+    if place == []:
+        abort(404)
+    reviews = [y.to_dict() for y in storage.all("Review").values()
+               if place_id == y.place_id]
+    return jsonify(reviews)
+
+
+@app_views.route('/reviews/<review_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_review(review_id):
+    '''Retrieves a particular Review object '''
+    reviews = storage.all("Review").values()
+    review = [x.to_dict() for x in reviews if x.id == review_id]
+    if review == []:
+        abort(404)
+    return jsonify(review[0])
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+                 strict_slashes=False)
+def create_review(place_id):
+    '''Creates a new Review object'''
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'user_id' not in request.get_json():
+        abort(400, 'Missing user_id')
+    user_id = request.json['user_id']
+    if 'text' not in request.get_json():
+        abort(400, 'Missing text')
+    places = storage.all("Place").values()
+    place = [x.to_dict() for x in places if x.id == place_id]
+    if place == []:
+        abort(404)
+    users = storage.all("User").values()
+    user = [y.to_dict() for y in users if y.id == user_id]
+    if user == []:
+        abort(404)
+    reviews = []
+    new_review = Review(text=request.json['text'], place_id=place_id,
+                        user_id=user_id)
+    storage.new(new_review)
+    storage.save()
+    reviews.append(new_review.to_dict())
+    return jsonify(reviews[0]), 201
+
+
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_review(review_id):
+    '''Deletes a Review object based in review_id'''
+    reviews = storage.all("Review").values()
+    review = [x.to_dict() for x in reviews if x.id == review_id]
+    if review == []:
+        abort(404)
+    for y in all_reviews:
+        if y.id == review_id:
+            storage.delete(y)
+            storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_review(review_id):
+    '''Updates a Review object using review_id'''
+    reviews = storage.all("Review").values()
+    review = [x.to_dict() for x in reviews if x.id == review_id]
+    if review == []:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    data = request.get_json()
+    ignore_keys = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(review, key, value)
+    review.save()
+    return jsonify(review.to_dict()), 200
